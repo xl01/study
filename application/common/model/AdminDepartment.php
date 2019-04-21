25abbb6498aa37e8c3a461a0150cf0f94bf6859f
xiaoliu <liuyw250@aliyun.com>
2019-04-21 19:40:20 +0800

    test

--- application/common/model/AdminDepartment.php
+++ application/common/model/AdminDepartment.php
@@ -1,14 +1,7 @@
 <?php
 namespace app\common\model;
-
 use think\Model;
-
-class AdminDepartment extends Model{
-    protected $table='my_admin_department';
-
-    protected $name='admin_department';
-
-    public  function getInfoByOpenid($open_id){
-        return $this->where('weixin_openid',$open_id)->find();
-    }
+class AdminDepartment extends Model
+{
+    protected $table = "my_admin_department";
 }
--- application/common/model/AdminUser.php
+++ application/common/model/AdminUser.php
@@ -3,24 +3,24 @@ namespace app\common\model;
 
 use think\Model;
 
-class AdminUser extends Model{
-    protected $table='my_admin_user';
-    protected $name='adminUser';
+class AdminUser extends Model
+{
+    protected $table = "my_admin_user";
 
-    //定义根据 openid获取数据的方法
-    public  function getInfoByOpenid($open_id){
+    public function getInfoByOpenid($open_id){
         return $this->where('weixin_openid',$open_id)->find();
     }
 
-    public function getDep(){
-        return $this->belongsTo('AdminDepartment','department_id','id');
+
+    //方法一
+    public function getDepartmentIdAttr($value,$data)
+    {
+    return $this->belongsTo('AdminDepartment','department_id','id')->where('id',$value)->value('name');
     }
-    //复杂的关联获取器 方法一
-    // public function getDepartmentIdAttr($value,$data){
-    //     return $this->belongsTo('AdminDepartment','department_id','id')->where('id',$value)->value('name');
+    //方法二
+    // public function getDepartmentIdAttr($value,$data)
+    // {
+    // return $this->belongsTo('AdminDepartment','department_id','id')->
+    // where('id',$data['department_id'])->value('name');
     // }
-    //复杂的关联获取器 方法二
-    public function getDepartmentIdAttr($value,$data){
-        return $this->belongsTo('AdminDepartment','department_id','id')->where('id',$data['department_id'])->value('name');
-    }
 }
