3e9927aaf4936031b4f2dfdc97134202c652cdad
xiaoliu <liuyw250@aliyun.com>
2019-04-26 22:01:04 +0800

    0426

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,15 +12,18 @@ class Index extends Base
     //手机页面添加项目
     $validate_name = "base/ProjectEnty.edit";
     $model_name='base/Project';
-    halt(json($this->editData()));
+        halt(json($this->editData()));
     }
     public function province(){
         return $this->fetch();
     }
     public function save(){
-        $save_data=['name'];
+        $param=[
+            'id'=>'id',
+            'name'=>'name'
+        ];
         // $validate_name = "base/ProjectEnty.edit";
         $model_name='base/Province';
-        halt($this->editData(false,false,$model_name,$save_data));
+        halt($this->editData($param,false,$model_name));
     }
 }
